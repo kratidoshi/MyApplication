132bdd661b9afbe0dc973e968b36f46396497692
krati <kratidoshi74@gmail.com>
2018-01-09 17:10:26 +0530

    commit1

---
+++ feature/src/main/java/diaries/om/com/myapplication/feature/MainActivity.java
@@ -0,0 +1,28 @@
+package diaries.om.com.myapplication.feature;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(in);
+            }
+        });
+
+        // ATTENTION: This was auto-generated to handle app links.
+
+    }
+}
---
+++ feature/src/main/java/diaries/om/com/myapplication/feature/SecondActivity.java
@@ -0,0 +1,19 @@
+package diaries.om.com.myapplication.feature;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Info_14 on 1/9/2018.
+ */
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+}
